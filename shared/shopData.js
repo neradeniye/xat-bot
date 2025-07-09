2050b97165c6c25d0d8f290a8dd7b88c2f11a184
Nu Dark <neradeniye@gmail.com>
2025-07-09 13:07:13 -0500

    Bot Update

--- shared/shopData.js
+++ shared/shopData.js
@@ -7,9 +7,9 @@ module.exports = {
     { name: 'Green', roleId: '1386977746361520148' },
     { name: 'Blue', roleId: '1386977749721157712' },
     { name: 'Indigo', roleId: '1386977752334205009' },
-    { name: 'Violet', roleId: '1386977755748634635' }
+    { name: 'Violet', roleId: '1386977755748634635' },
   ],
   items: [
-    { name: 'Pink Pawn', price: 500, roleId: '1392263894449524767' }
+    { name: 'Pink Pawn', price: 500, roleId: '1392263894449524767' },
   ]
 };
