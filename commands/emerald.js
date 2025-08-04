0d7787fe4a8192e9e5477f026dbf72bf472d19df
Nu Dark <neradeniye@gmail.com>
2025-08-04 02:41:40 -0500

    Bot Update

--- commands/emerald.js
+++ commands/emerald.js
@@ -23,7 +23,7 @@ export default {
     }
 
     // Check that Emerald is enabled (via .x enable emerald)
-    if (!setItemEnabled(userId, 'emerald pawn')) {
+    if (!setItemEnabled(userId, emeraldPawn.name)) {
       return message.reply('⚠️ You must enable **emerald** first using `.x enable emerald`.');
     }
 
