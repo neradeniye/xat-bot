1173cdece0dbe625912062aa9a1bd6c1e8d80be0
Nu Dark <neradeniye@gmail.com>
2025-11-09 00:59:58 -0600

    Bot Update

--- commands/profile.js
+++ commands/profile.js
@@ -95,7 +95,7 @@ export default {
     ctx.beginPath();
     ctx.arc(150, 115, 75, 0, Math.PI * 2);
     ctx.clip();
-    ctx.drawImage(avatar, 75, 40, 100, 150);
+    ctx.drawImage(avatar, 35, 40, 150, 150);
     ctx.restore();
     ctx.lineWidth = 6;
     ctx.strokeStyle = 'white';
