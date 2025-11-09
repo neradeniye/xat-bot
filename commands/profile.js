14b63217f0c5dc66df065676e25ec1182471ce4a
Nu Dark <neradeniye@gmail.com>
2025-11-09 01:10:35 -0600

    Bot Update

--- commands/profile.js
+++ commands/profile.js
@@ -122,7 +122,7 @@ export default {
     ctx.drawImage(heartImg, 40, 210, 40, 40);
     ctx.fillStyle = 'white';
     ctx.font = 'bold 32px Arial';
-    ctx.fillText('Single', 200, 120);
+    ctx.fillText('Single', 250, 150);
 
     // Coins + balance
     const coinsImg = await loadImg(EMOJI.coins);
