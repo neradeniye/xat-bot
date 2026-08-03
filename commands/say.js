120741d710d073a450cef6f88504f98c7afc0720
neradeniye <neradeniye@gmail.com>
2026-08-03 12:12:53 -0500

    Bot Update

--- commands/say.js
+++ commands/say.js
@@ -76,7 +76,7 @@ export default {
 
       // Send as the user + small identification
       await webhook.send({
-        content: `${text}\n\n- <@${member.id}>`,
+        content: `${text} <@${member.id}>`,
         username: member.displayName,
         avatarURL: member.displayAvatarURL({ dynamic: true, size: 256 }),
         allowedMentions: { parse: [] }, // still blocks @everyone / @here, but the <@id> still shows as a clickable name
